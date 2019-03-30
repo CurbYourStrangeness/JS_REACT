b21b3e17093ec628af3c6693202fe98acf45b7d5
CurbYourStrangeness <devbuild2018@gmail.com>
2019-03-29 20:08:09 -0600

    modified App.js to use the newly created HelloWorld files, some issues with loading css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import HelloWorld from './HelloWorld';
 import './App.css';
 
 const App = () => {
-  return (<div classname="App">Hello World!</div>);
+  return (<div classname="App"><HelloWorld/></div>);
 };
 
 export default App;
